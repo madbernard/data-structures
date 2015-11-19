80e7b0cea60f17b30fb094b5d6d9a2caef1bd979
Mad Bernard <madeline.a.bernard@gmail.com>
2015-11-19 11:30:29 -0800

    Functional stack and queue pass all tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,7 @@
 var Queue = function(){
   var someInstance = {};
+  var count = 0;
+  var sizeCounter = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,13 +9,27 @@ var Queue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[count] = value;
+    count += 1;
+    sizeCounter += 1;
   };
 
   someInstance.dequeue = function(){
+    if (sizeCounter) {
+      //refer to last key
+      var result = storage[count - sizeCounter];
+      delete storage[count - sizeCounter];
+      sizeCounter -= 1;
+      return result;
+    }
   };
 
   someInstance.size = function(){
+    return sizeCounter;
   };
 
   return someInstance;
 };
+
+
+
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,28 +1,30 @@
 var Stack = function(){
   var someInstance = {};
   var count = 0;
+  var sizeCounter;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
-    count +=1;  //every time we push one, the size increases
-     //create a key value pair within storage. we need to add the value to storage
-     storage.count = value;  //storage has now a property and a value attached to that property
+    count +=1;                       //every time we push one, the size increases
+                                     //create a key value pair within storage. we need to add the value to storage
+     storage[count] = value;          //storage has now a property and a value attached to that property
 
   };
 
   someInstance.pop = function(){
 
-    var result = storage.count;  //get the results (value) stored
-    delete storage.count;//delete the key value pair
-    //return the result. we will put this at the bottom
-
-    if (count) {  //this will make sure that the count stops at zero
-      count -=1;  //every time we push one, the size decreases
+    if (count) {                     //it will make sure that the count stops at zero
+          var result = storage[count];      //get the results (value) stored
+    delete storage[count];            //delete the key value pair
+      count -=1;
+                                     //return the result. we will put it at the bottom
+    return result;                   //alternatively, we can delete the first two line of it function
+                     //every time we push one, the size decreases
     }
-    return result;
+                                     //and replace it line with return storage.count
   };
 
   someInstance.size = function(){
@@ -31,3 +33,4 @@ var Stack = function(){
 
   return someInstance;
 };
+
