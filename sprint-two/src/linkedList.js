dc3a247a1e6694e9a14889a9405789f304a774a0
Mad Bernard <madeline.a.bernard@gmail.com>
2015-11-20 11:54:19 -0800

    some of linked list done

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,19 +3,48 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
+
   list.addToTail = function(value){
+    if (list.head){
+      list.tail = linkedListNode(value);
+    }
+    else {
+      list.head = linkedListNode(value);
+      list.tail = list.head;
+    }
   };
 
   list.removeHead = function(){
+
+// save the current value of the head as formerhead
+// go to the next node (node.next)
+// reassign (node.next) to be list.head
+// return former head
+   var formerHead = list.head;
+   list.head = formerHead.next;
+   return formerHead.value;
   };
 
   list.contains = function(target){
+    // loop on the entire list comparying values
+    // for loop without i
+    //first part is starting position
+    //middle part is position at which it is thrown out
+    //last part is how it advances
+
+    for ( var x = list.head.value; list.tail.next === null; list.head.next = list.tail) {
+      if ( x === target){
+        return true;
+      }
+      else return false;
+    }
+
   };
 
   return list;
 };
 
-var Node = function(value){
+var linkedListNode = function(value){
   var node = {};
 
   node.value = value;
