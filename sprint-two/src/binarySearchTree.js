8ff54883e4bd879a0506f6992ded73becac58c71
Mad Bernard <madeline.a.bernard@gmail.com>
2015-11-21 17:36:12 -0800

    Binary Search Tree done excellently

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,53 +1,109 @@
 var BinarySearchTree = function(value){
   var newTree = _.extend({}, binarySearchTreeMethods);
   newTree.value = value;    //creates a key value pair with value as parameter
-  newTree.left = [];
-  newTree.right = [];
+  newTree.left = null;
+  newTree.right = null;
   return newTree;
 };
 
 
 var binarySearchTreeMethods = {};
 
+//5 2 3 7 6
+/*
+        5
+    7       2
+      6   3
+
+
+*/
+
+
 binarySearchTreeMethods.insert = function(value){
-  // assign the BinarySearchTree(value) just like the newTree in the constructor did
   // check if value is bigger than this.value then...
-
+  if (value > this.value) {
     // ...check if there is a tree to the right
-      // advance into that new tree and run `insert` again
-      // if not... build a new tree to the right
-
-    // otherwise check if there is a tree to the left
-      // advance into that new tree and run `insert` again
-      // if not... build a new tree to the left
-
-  this.children.push(BinarySearchTree(value));   //calling the constructor again to assign value
-
-};
-
-binarySearchTreeMethods.contains = function(target){
-  // we need to traverse through the entire tree
-  var found = false;
-
-  function search (node) {
-    // base case if the value is the target or if there are no children
-    if (node.value === target){
-      found = true;
-      return;
+    if (this.right !== null) {
+      // advance into that new tree
+      // and run `insert` again
+      this.right.insert(value);
     }
-
-    // recursive case if there are children
-    for ( var i = 0; i < node.children.length; i++) {
-      search(node.children[i]);
+    else {
+      // if not... build a new tree to the right
+      //(include value plus binaryTreeSearch constructor)
+      this.right = BinarySearchTree(value);
     }
   }
-
-  search(this);
-
-  return found;
+  else {
+    // otherwise check if there is a tree to the left
+    // if not... build a new tree to the left
+        //(include value plus binaryTreeSearch constructor)
+      if (this.left !== null) {
+      // advance into that new tree
+      // and run `insert` again
+      this.left.insert(value);
+    }
+    else {
+      // if not... build a new tree to the left
+      //(include value plus binaryTreeSearch constructor)
+      this.left = BinarySearchTree(value);
+    }
+  }
 };
 
-binarySearchTreeMethods.depthFirstLog = function(){};
+//5 2 3 7
+/*
+        5
+    7       2
+         3
+
+
+*/
+
+binarySearchTreeMethods.contains = function(target){
+  if (this.value === target) {
+    return true;
+  }
+
+  if (this.value < target) {
+    // go to right
+    if (this.right){
+      return this.right.contains(target);
+    }
+    return false;
+  }
+  else {
+    if (this.left){
+      return this.left.contains(target);
+    }
+    return false;
+  }
+
+};
+
+binarySearchTreeMethods.depthFirstLog = function(cb){
+
+  // if: both this.right and this.left are null...
+  if (!this.right && !this.left){
+  // ... call the cb and return
+    cb(this.value);
+    return;
+  }
+
+  //call the callback in this.value
+  cb(this.value);
+    // recursive case if there are children
+    // call depthFirstLog in both sides at once (if we can)
+  if (this.right) {
+    this.right.depthFirstLog(cb);
+  }
+  if (this.left) {
+    this.left.depthFirstLog(cb);
+  }
+    //  if not, left side, then right side.
+    // return at the bottom
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
