cccc422332dbcad733dfbc1db6ff86f050ee5a7f
Mad Bernard <madeline.a.bernard@gmail.com>
2015-11-29 12:28:04 -0800

    tree methods are extended into it, addChild works

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,8 +3,8 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -15,10 +15,27 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
+  // The trick is, how to haul a "true" out from a branch?
+  // last time, we used a subroutine (interior recursing function) with a closure variable,
+  // that only changed when a true was found, and was returned at the end
+
+  // I know it's possible to do this by recursing the .contains, though.
+  // With reduce?  There was a telltale variable returned?
+
+  var telltale =
+
+  // base case
+  if (this.value === target) {
+    return true;
+  }
+  // other base case
+  else if {
+
+  }
 
 };
 
