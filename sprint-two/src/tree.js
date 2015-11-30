3679cc4f4da78751a2b97a68ba382cc92e04b41c
Mad Bernard <madeline.a.bernard@gmail.com>
2015-11-29 20:35:03 -0800

    much tidier recursive tree contains now working

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,3 +1,8 @@
+// Tree is a function that creates an object,
+// that has the key "value" which holds a string,
+// and the key "treeMethods" which holds an objects full of function references,
+// and the key "children" which holds an array of Tree-constructed objects
+
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
@@ -8,10 +13,6 @@ var Tree = function(value){
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
@@ -20,23 +21,29 @@ treeMethods.addChild = function(value){
 
 treeMethods.contains = function(target){
   // The trick is, how to haul a "true" out from a branch?
-  // last time, we used a subroutine (interior recursing function) with a closure variable,
-  // that only changed when a true was found, and was returned at the end
+  // last time, we used a subroutine (interior recursing function),
+  // with a closure variable that only changed when a true was found, and was returned at the end
 
   // I know it's possible to do this by recursing the .contains, though.
   // With reduce?  There was a telltale variable returned?
-
-  var telltale =
 
   // base case
   if (this.value === target) {
     return true;
   }
-  // other base case
-  else if {
-
+  else {
+    // this.children is an array with at least one Tree object in it
+    // if recursion ever returns true gtfo ie return true
+    // if recursion returns false move on to the next possibility
+    // if the for loop closes return false
+    for (var i = 0; i < this.children.length; i++) {
+      // recurse
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+    return false;
   }
-
 };
 
 
